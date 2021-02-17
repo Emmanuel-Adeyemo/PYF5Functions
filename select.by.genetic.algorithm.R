cb1bed4ab6a531d3a43cf5df2f3c6522b3d28224
Emmanuel Adeyemo <eadeyemo2@gmail.com>
2021-02-17 16:37:58 -0600

    Create select.by.genetic.algorithm.R

---
+++ select.by.genetic.algorithm.R
@@ -0,0 +1,26 @@
+select.by.genetic.algorithm <- function(validation_genotpe, num_to_select, method){
+    
+    #'@ method: options are "AOPT", "DOPT", "PEVMEAN", "CDMEAN" and so on. See STPGA manual for details
+    
+    rownames(validation_genotpe) <- validation_genotpe[, 1]
+    validation_genotpe <- validation_genotpe[,-1]
+    
+    M <- validation_genotpe
+    Kmat<-cov(t(M))
+    Kmat<-Kmat/mean(diag(Kmat))
+    dim(Kmat)
+    svdG<-svd(Kmat, nu=40,nv=40)
+    PCAs<-Kmat%*%svdG$v
+    rownames(PCAs)<-rownames(Kmat)
+
+    ListTrain1<-GenAlgForSubsetSelectionNoTest(P=PCAs,ntoselect=num_to_select, InitPop=NULL,
+                                                npop=500, nelite=10, mutprob=.5, mutintensity = 1,
+                                                niterations=800,minitbefstop=20, tabu=F,tabumemsize = 0,
+                                                plotiters=F, lambda=1e-9,errorstat=method, mc.cores=1)
+    X <- ListTrain1[[1]]
+    out <- as.data.frame(X)
+    colnames(out) <- "ID"
+    
+    return(out)
+    
+}

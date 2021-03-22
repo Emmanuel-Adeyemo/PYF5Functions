cba3361069e65b6e34145eecf1c9183f1f344fd1
Emmanuel Adeyemo <eadeyemo2@gmail.com>
2021-03-22 14:06:40 -0500

    Update select.by.genetic.algorithm.R

--- select.by.genetic.algorithm.R
+++ select.by.genetic.algorithm.R
@@ -1,7 +1,12 @@
 select.by.genetic.algorithm <- function(validation_genotpe, num_to_select, method){
     
-    #'@param method: options are "AOPT", "DOPT", "PEVMEAN", "CDMEAN" and so on. See STPGA manual for details
-    #' output is a dataframe of optimized lines ie TP
+    #'@ method: options are "AOPT", "DOPT", "PEVMEAN", "CDMEAN", "CDMEANMM (for multitrait)"
+    
+    if(method == "CDMEANMM"){        
+        Lambda <- 1
+    }else{
+        Lambda <- 1e-9
+    }
     
     rownames(validation_genotpe) <- validation_genotpe[, 1]
     validation_genotpe <- validation_genotpe[,-1]
@@ -13,11 +18,16 @@ select.by.genetic.algorithm <- function(validation_genotpe, num_to_select, metho
     svdG<-svd(Kmat, nu=40,nv=40)
     PCAs<-Kmat%*%svdG$v
     rownames(PCAs)<-rownames(Kmat)
-
-    ListTrain1<-GenAlgForSubsetSelectionNoTest(P=PCAs,ntoselect=num_to_select, InitPop=NULL,
-                                                npop=500, nelite=10, mutprob=.5, mutintensity = 1,
-                                                niterations=800,minitbefstop=20, tabu=F,tabumemsize = 0,
-                                                plotiters=F, lambda=1e-9,errorstat=method, mc.cores=1)
+    
+    if(method == "CDMEAN" | method == "CDMEANMM"){
+        Psolve <- solve(Kmat+1e-6*diag(ncol(Kmat)))
+    }else{
+        Psolve <- PCAs
+    }
+    ListTrain1<-GenAlgForSubsetSelectionNoTest(P=Psolve,ntoselect=num_to_select, InitPop=NULL,
+                                                npop=200, nelite=10, mutprob=.5, mutintensity = 1,
+                                                niterations=500,minitbefstop=20, tabu=F,tabumemsize = 0,
+                                                plotiters=F, lambda=Lambda,errorstat=method, mc.cores=1) 
     X <- ListTrain1[[1]]
     out <- as.data.frame(X)
     colnames(out) <- "ID"
